0a1da0338ced0dbc06f7a895ca3e916fe6f90262
pyxyzc <pyxyzc@gmail.com>
2025-12-29 04:53:30 +0000

    Fix cmake error

--- build.sh
+++ build.sh
@@ -1,9 +1,23 @@
 #!/usr/bin/bash
 
-# the legacy way of using cmake
-# export MY_INCLUDE_PATH="/usr/local/lib/python3.12/dist-packages/torch/include:/usr/local/lib/python3.12/dist-packages/torch/include/torch/csrc/api/include:/usr/include/python3.12:/usr/local/cuda/include"
-# [[ -d ./build/ ]] && rm -rf ./build/
-# cmake -Dpybind11_DIR="/usr/local/lib/python3.12/dist-packages/pybind11/share/cmake/pybind11" -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE=/root/hk/ucm/myenv/bin/python -DCMAKE_PREFIX_PATH="/usr/local/lib/python3.12/dist-packages/pybind11/share/cmake/pybind11" -DCMAKE_PREFIX_PATH=$(python3 -c "import torch;print(torch.utils.cmake_prefix_path)") -DRUNTIME_ENVIRONMENT=cuda -S . -B ./build
-# cd build
-# make
-# cd ..
+export MY_INCLUDE_PATH="\
+/usr/local/lib/python3.12/dist-packages/torch/include:\
+/usr/local/lib/python3.12/dist-packages/torch/include/torch/csrc/api/include:\
+/usr/include/python3.12/:\
+/usr/local/cuda/include"
+
+if [[ -d ./build ]]; then
+	rm -rf ./build
+fi
+
+cmake -S . -B build \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DRUNTIME_ENVIRONMENT=cuda \
+	-DPYTHON_EXECUTABLE=/usr/local/lib/python3.12/ \
+	-Dpybind11_DIR=/usr/local/lib/python3.12/dist-packages/pybind11/share/cmake/pybind11 \
+	-DTorch_DIR=/usr/local/lib/python3.12/dist-packages/torch/share/cmake/Torch \
+	-DCMAKE_CUDA_COMPILER=/usr/local/cuda/bin/nvcc
+
+cd build
+make
+cd ..
